058919930a4d178004ccba806138ab612ce880f2
Bartek <bartlomiej.proczkowski@gmail.com>
2021-08-19 21:54:29 +0200

    Frontend proxy to Deployment

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,15 +2,19 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  getTopProducts,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
